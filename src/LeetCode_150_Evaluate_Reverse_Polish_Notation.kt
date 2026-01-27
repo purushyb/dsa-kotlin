90f953bc18ac8a96a7565b334356a7d81cfa13c2
purushyb <purushybw@gmail.com>
2026-01-27 22:48:29 +1100

    [2026-01-19]: Added code to evaluate reverse polish notation

--- src/LeetCode_150_Evaluate_Reverse_Polish_Notation.kt
+++ src/LeetCode_150_Evaluate_Reverse_Polish_Notation.kt
@@ -1,2 +1,38 @@
-class LeetCode_150_Evaluate_Reverse_Polish_Notation {
+import java.util.ArrayDeque
+
+object LeetCode_150_Evaluate_Reverse_Polish_Notation {
+    fun evalRPN(tokens: Array<String>): Int {
+        val stack = ArrayDeque<Int>()
+        for(i in tokens) {
+
+            if(i == "*" || i == "-" || i == "+" || i == "/") {
+                val num1 = stack.pop()
+                val num2 = stack.pop()
+                when(i) {
+                    "*" -> {
+                        stack.push(num2 * num1)
+                    }
+                    "+" -> {
+                        stack.push(num2 + num1)
+                    }
+                    "-" -> {
+                        stack.push(num2 - num1)
+                    }
+                    "/" -> {
+                        stack.push(num2 / num1)
+                    }
+                }
+            }
+            else{
+                stack.push(i.toInt())
+            }
+
+        }
+        return stack.pop()
+    }
+}
+
+fun main() {
+    val input = arrayOf<String>("4","-2","/","2","-3","-","-")
+    println(LeetCode_150_Evaluate_Reverse_Polish_Notation.evalRPN(input))
 }
--- src/LeetCode_278_First_Bad_Version.kt
+++ src/LeetCode_278_First_Bad_Version.kt
@@ -1,2 +1,46 @@
-class LeetCode_278_First_Bad_ {
+class LeetCode_278_First_Bad_Version: VersionControl() {
+    override fun firstBadVersion(n: Int) : Int {
+        var low = 1
+        var high = n
+
+        while(low < high) {
+            val mid = low + ( high - low) / 2
+
+            if(isBadVersion(mid)) {
+                high = mid
+            }
+            else {
+                low = mid + 1
+            }
+        }
+
+        return low
+
+    }
+
+    private fun binarySearchForElement(low: Int, high: Int): Int {
+        if(low <= high) {
+            val mid = low + (high - low) / 2
+            return if(isBadVersion(mid)) {
+                if(!isBadVersion(mid - 1)) mid
+                else binarySearchForElement(low, mid - 1)
+            } else {
+                binarySearchForElement(mid + 1, high)
+            }
+        }
+        return -1
+    }
+}
+
+abstract class VersionControl {
+    open fun firstBadVersion(n: Int) : Int { return 0}
+
+    fun isBadVersion(version: Int): Boolean {
+        println("Checking for $version")
+        return version >= 4
+    }
+}
+
+fun main() {
+    println(LeetCode_278_First_Bad_Version().firstBadVersion(5))
 }
