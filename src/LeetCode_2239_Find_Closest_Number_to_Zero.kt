98823d9068ae9f4f5ac4a38e842051f154bc6fef
purushyb <purushybw@gmail.com>
2026-03-19 09:25:37 +1100

    [2026-03-17][2239]: Added code to find closet numbers to zero in array

--- src/LeetCode_2239_Find_Closest_Number_to_Zero.kt
+++ src/LeetCode_2239_Find_Closest_Number_to_Zero.kt
@@ -1,2 +1,25 @@
+import kotlin.math.abs
+
 class LeetCode_2239_Find_Closest_Number_to_Zero {
+    fun findClosestNumber(nums: IntArray): Int {
+        var nn = Int.MAX_VALUE
+        var d = Int.MAX_VALUE
+
+        nums.forEach {
+            val nd = abs(0 - it)
+            if(nd < d || nd == d && it > nn) {
+                nn = it
+                d = nd
+            }
+        }
+
+        return nn
+    }
+}
+
+fun main() {
+    val problem = LeetCode_2239_Find_Closest_Number_to_Zero()
+    val input = intArrayOf(-4,-2,1,4,8)
+    val result = problem.findClosestNumber(input)
+    println(result)
 }
